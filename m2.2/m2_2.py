d9735205bcb1bbad82ef166df148c74d22c67401
ANISH_KARTHIC <anishkarthicvs@gmail.com>
2025-08-27 15:54:30 +0530

    Update m2_2.py

--- m2.2/m2_2.py
+++ m2.2/m2_2.py
@@ -1,3 +1,10 @@
 def guessing_game_with_input():
-    number = input("Enter the number you guess:")
-    print("You guessed", number)
+    
+    print("G U E S S I N G    G A M E")
+    print("--------------------------")
+    print("Please think of a number (between 3 and 3) ")
+    input("Press enter when you are ready!...")
+
+    resp = input("Is it 3? ")
+    print("Number 3 guessed in 1 tries!")
+    print("Thank you for playing!")
